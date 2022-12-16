f528e58e60f3dfe37f3096684f2785790de634ae
«Artur» <art13471118@gmail.com>
2022-12-16 14:43:30 +0300

    Программа, где задается массив из ранжомных чисел и находится сумма элементов, стоящих на нечетных позициях

---
+++ Task36/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] Random(int num, int a, int b)
+{
+    int[] arr = new int[num];
+    for (int i = 0; i < num; i++)
+    {
+        arr[i] = new Random().Next(a, b + 1);
+    }
+    return arr;
+}
+
+void Print(int[] arr2)
+{
+    for (int i = 0; i < arr2.Length; i++)
+    {
+        Console.Write($"{arr2[i]} ");
+    }
+}
+
+int[] array = Random(4, -100, 100);
+Print(array);
+Console.WriteLine();
+int num = 0;
+for (int i = 1; i < array.Length; i = i + 2)
+{
+    {
+        num += array[i];
+    }
+}
+Console.WriteLine(num);
