4414f1275d4f70d30b694d3a001e8478f9900c2c
«Artur» <art13471118@gmail.com>
2022-12-16 14:09:28 +0300

    Программа, где зажается массив из случайных трехзначных положительных чисел и считается количество четных чисел в массиве

---
+++ Task34/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите
+// программу, которая покажет количество чётных чисел в массиве. [345, 897, 568, 234] -> 2
+
+int[] Random(int num)
+{
+    int[] array = new int[num];
+    for (int i = 0; i < num; i++)
+    {
+        array[i] = new Random().Next(0, 1000);
+    }
+    return array;
+}
+
+void Print(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+Console.Write("Введите число элементов массива: ");
+int N = int.Parse(Console.ReadLine()!);
+
+int[] array = Random(N);
+Print(array);
+Console.WriteLine();
+int a = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] % 2 == 0)
+    {
+        a += 1;
+    }
+}
+Console.WriteLine(a);
