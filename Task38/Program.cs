26f69da20f053cab41c500d0f69663dd32973f3b
«Artur» <art13471118@gmail.com>
2022-12-16 16:50:08 +0300

    Программа, где задается массив и выводится разница между максимальным и минимальным элементами массива

---
+++ Task38/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+void Array1(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-211123, 20039829);
+    }
+}
+
+void Array2(double[] print)
+{
+    for (int i = 0; i < print.Length; i++)
+    {
+        Console.Write($"{print[i]} ");
+    }
+}
+
+double[] array = new double[10];
+Array1(array);
+Array2(array);
+Console.WriteLine();
+double max = array[0];
+double min = array[0];
+
+for (int i = 1; i < array.Length; i++)
+{
+    if (array[i] > max)
+    {
+        max = array[i];
+    }    
+  if (array[i] < min)
+{
+        min = array[i];
+}
+}
+double a = max - min;
+Console.WriteLine($"Разница между максимальным и минимальным элементами равна = {a}");
